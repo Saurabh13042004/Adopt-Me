213142f57bd4591e6edef6d6d0074c4acd683936
Saurabh Shukla <99032013+Saurabh13042004@users.noreply.github.com>
2023-08-27 15:07:04 +0530

    breedList-updated

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -1,38 +1,10 @@
-import { useState, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+import fetchBreedList from "./fetchBreedList";
 
-const localCache = {};
 
 //Custom Hooks other hooks packaged together 
 export default function useBreedList(animal) {
-    const [breedList, setBreedsList] = useState([]);
-
-    const [status, setStatus] = useState("unloaded");
-
-    useEffect(() => {
-        if (!animal) {
-            setBreedsList([]);
-        }
-        else if (localCache[animal]) {
-            setBreedsList(localCache[animal])
-        }
-        else {
-            requestBreedList();
-        }
-
-        async function requestBreedList() {
-            setBreedsList([]);
-            setStatus("Loading");
-
-            const res = await fetch(
-                `https://pets-v2.dev-apis.com/breeds?animal=${animal}`
-            )
-            const json = await res.json();
-            localCache[animal] = json.breeds || []
-            setBreedsList(localCache[animal]);
-            setStatus("Loading");
-
-        }
-    }, [animal])
+    const results = useQuery(["breeds",animal],fetchBreedList)
     
-    return [breedList, status];
+    return [results?.data?.breeds??[],results.status];
 }    
