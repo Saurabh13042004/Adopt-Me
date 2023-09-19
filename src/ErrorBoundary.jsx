cf5f3bf6b6fc96e13b2a14df438dd2463e6a3d65
Saurabh Shukla <99032013+Saurabh13042004@users.noreply.github.com>
2023-09-19 17:45:31 +0530

    error-boundary included

---
+++ src/ErrorBoundary.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+
+class ErrorBoundary extends Component {
+    state = { hasError: false };
+    static getDerivedStateFromError() {
+        return { hasError: true };
+    }
+    componentDidCatch(error, info) {
+        console.error("ErrorBoundary caught an error", error, info);
+        
+    }
+}
+
+
